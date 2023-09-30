5c95030b73b0a7f9e4ed0c2f4e3cc5fa491cb494
Gabriel Miranda <gabrielmirandazacarias@gmail.com>
2023-09-30 16:25:05 -0300

    Primeiro codigo em Java

---
+++ Fundamentos/funda/src/App.java
@@ -0,0 +1,15 @@
+import Fundamentos.licoes.test01;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        test01 t = new test01();
+
+        System.out.println(t.getIdade());
+
+        System.out.println("Faz o maldito L seu canalha\n" + "Vou fazer porra nenhuma seu verme\n");
+
+
+    }
+}
---
+++ Fundamentos/licoes/test01.java
@@ -0,0 +1,9 @@
+package Fundamentos.licoes;
+
+public class test01 {
+    private int idade = 30;
+
+    public int getIdade(){
+        return this.idade;
+    }
+}
